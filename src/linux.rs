fdd080dd54e850f5b0c7c618d1b7621b80217a1e
temidaradev <temidaradev@proton.me>
2026-04-07 15:02:01 +0300

    linux infos

--- src/linux.rs
+++ src/linux.rs
@@ -1,15 +1,6 @@
 use std::fs::{self, File};
 use std::io::{BufRead, BufReader};
 
-#[derive(Debug)]
-pub struct Linux {
-    hostname: String,
-    os: String,
-    kernel: String,
-    cpu: String,
-    memory: String,
-}
-
 fn get_os_info() -> Option<String> {
     let file = File::open("/etc/os-release").ok()?;
     let reader = BufReader::new(file);
@@ -25,11 +16,56 @@ fn get_os_info() -> Option<String> {
     None
 }
 
+fn get_cpu_info() -> Option<String> {
+    let file = std::fs::read_to_string("/proc/cpuinfo").ok()?;
+
+    let mut model = None;
+
+    for line in file.lines() {
+        if line.starts_with("model name") && model.is_none() {
+            model = line.split(':').nth(1).map(|s| s.trim().to_string());
+        }
+    }
+
+    Some(format!(
+        "{}",
+        model.unwrap_or("Unknown".into()),
+    ))
+}
+
+fn get_mem_info() -> Option<String> {
+    let file = std::fs::read_to_string("/proc/meminfo").ok()?;
+
+    let mut mem = None;
+
+    for line in file.lines() {
+        if line.starts_with("MemTotal") && mem.is_none() {
+            mem = line.split(':').nth(1).map(|s| s.trim().to_string());
+        }
+    }
+    
+    if let Some(mem_str) = mem {
+        let kb_value = mem_str.split_whitespace().next()?.parse::<f64>().ok()?;
+
+        let gb_value = kb_value / 1_048_576.0;
+
+        return Some(format!("{:.2} GiB", gb_value));
+    }
+
+    Some("Memory: Unknown".into())
+}
+
 pub fn show_info() {
     if let Ok(os) = fs::read_to_string("/etc/hostname") {
         println!("\x1b[34mHost:\x1b[0m {}", os.trim());
     }
     if let Some(os) = get_os_info() {
         println!("\x1b[34mOS:\x1b[0m {}", os);
+    }
+    if let Some(os) = get_cpu_info() {
+        println!("\x1b[34mCPU:\x1b[0m {}", os);
+    }
+    if let Some(os) = get_mem_info() {
+        println!("\x1b[34mMemory:\x1b[0m {}", os);
     }
 }
